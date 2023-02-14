9a1178fc3907184b863e3cac1f8a9a810ae210b4
Joonas Kemppainen <karmakebu@gmail.com>
2023-02-14 13:00:45 +0200

    added validations + fixed some bugs

--- index.js
+++ index.js
@@ -82,7 +82,10 @@ app.put("/api/people/:id", (req, res, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, person, { 
+    new: true,
+    runValidators: true, 
+  })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
